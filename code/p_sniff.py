717ed856221e9d3be98fdf9a8ba168cbce7f3b5d
phoenix107 <root@localhost.localdomain>
2017-07-10 23:37:34 +0530

    Minor Degugging

--- code/p_sniff.py
+++ code/p_sniff.py
@@ -1,21 +1,28 @@
-from layer2 import * 
-from layer3 import *
+from layer_2 import * 
+from layer_3 import *
 
 
-while True:
 
-  sock = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
+sock = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
 
-  data = sock.recv(65535)
+while True: 
 
-  e_ob = Ether(data[0:14])
-  e_ob.edata()
+  try:
+    data = sock.recv(65535)
 
-  if(e_ob.e_pro == 'IP'):
-     i_ob = IP(data[14:34])
-     i_ob.idis()
+    e_ob = Ether(data[0:14])
+    e_ob.edata()
+
+    if(e_ob.e_pro == 'IP'):
+       i_ob = IP(data[14:34])
+       i_ob.idis()
   
-  elif(e_ob.e_pro == 'ARP'):
-    a_ob = ARP(data[14:42])
-    a_ob.adis()
+    elif(e_ob.e_pro == 'ARP'):
+      a_ob = ARP(data[14:42])
+      a_ob.adis()
+  
+  except KeyboardInterrupt:
+    print "Done Scanning"
+    sock.close()
+    break
   
